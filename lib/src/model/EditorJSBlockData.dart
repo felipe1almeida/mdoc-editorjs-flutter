0d995c254cb3edd7f217cf491511935bc9d61fb7
Felipe Oliveira <ext.felipe.oliveira@mdoc.one>
2023-11-02 15:54:41 +0000

    Change the code to make sure it handles maps

--- lib/src/model/EditorJSBlockData.dart
+++ lib/src/model/EditorJSBlockData.dart
@@ -11,24 +11,21 @@ class EditorJSBlockData {
   final bool? stretched;
   final bool? withBackground;
 
-  EditorJSBlockData(
-      {this.text,
-      this.level,
-      this.style,
-      this.items,
-      this.file,
-      this.caption,
-      this.withBorder,
-      this.stretched,
-      this.withBackground});
+  EditorJSBlockData({this.text, this.level, this.style, this.items, this.file, this.caption, this.withBorder, this.stretched, this.withBackground});
 
   factory EditorJSBlockData.fromJson(Map<String, dynamic> parsedJson) {
     var list = parsedJson['items'] as List?;
-    final List<String> itemsList = <String>[];
+    List<String> itemsList = <String>[];
 
     if (list != null) {
+      itemsList = [];
       list.forEach((element) {
-        itemsList.add(element);
+        if (element is String) {
+          itemsList.add(element);
+        } else if (element is Map<String, dynamic>) {
+          // If the element is a Map, convert it to a String and then add it.
+          itemsList.add(element.toString());
+        }
       });
     }
 
@@ -37,9 +34,7 @@ class EditorJSBlockData {
         level: parsedJson['level'],
         style: parsedJson['style'],
         items: itemsList,
-        file: (parsedJson['file'] != null)
-            ? EditorJSBlockFile.fromJson(parsedJson['file'])
-            : null,
+        file: (parsedJson['file'] != null) ? EditorJSBlockFile.fromJson(parsedJson['file']) : null,
         caption: parsedJson['caption'],
         withBorder: parsedJson['withBorder'],
         withBackground: parsedJson['withBackground']);
